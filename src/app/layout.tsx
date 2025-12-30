4f1b4e627d59e3b825425bbc40ec36162e080ba8
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2025-12-30 10:24:48 +0330

    added prettierrc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from "next";
-import { Vazirmatn } from 'next/font/google'
+import { Vazirmatn } from "next/font/google";
 
-
-const vazirmatn = Vazirmatn ({
-  subsets: ['latin','arabic'],
-  display:"swap",
-
-})
+const vazirmatn = Vazirmatn({
+  subsets: ["latin", "arabic"],
+  display: "swap",
+});
 import "./globals.css";
-
-
-
 
 export const metadata: Metadata = {
   title: "دکتر من",
@@ -23,12 +18,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="fa" dir="rtl"  className={vazirmatn.className}>
-      <body
-       
-      >
-        {children}
-      </body>
+    <html lang="fa" dir="rtl" className={vazirmatn.className}>
+      <body>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
-
-
 const Home = () => {
-  return (
-    <div className="font-normal">سلام رفیق!</div>
-  )
-}
+  return <div className="font-normal">سلام رفیق!</div>;
+};
 
-export default Home
+export default Home;
